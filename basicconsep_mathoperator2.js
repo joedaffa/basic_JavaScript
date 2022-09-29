c83eac4d25cfca1705a4a564f36d7cf32c01d1e3
joedaffa <kelvin.daffa04@gmail.com>
2022-09-29 23:00:32 +0700

    29_09_2022

---
+++ basicconsep_mathoperator2.js
@@ -0,0 +1,60 @@
+/*
+first operation;
+addition
+*/
+var a = 10
+var b = 10
+var c = a+b
+
+console.log(c)
+
+/*
+first operation;
+substraction
+*/
+var a = 25
+var b = 5
+var c = a-b
+
+console.log(c)
+
+/*
+first operation;
+multiplication
+*/
+var a = 10
+var b = 20
+var c = a*b
+
+console.log(c)
+
+/*
+first operation;
+division
+*/
+
+var a = 20
+var b = 10
+var c = a/b
+
+console.log(c)
+
+/*
+first operation;
+modulus
+*/
+
+var a = 56
+var b = 3
+var c = a%b
+
+console.log(c)
+ 
+var d = Math.sqrt(18);
+console.log(d)
+
+var e = Math.pow(2.2, 2);
+console.log(e)
+
+var f = Math.pow(2.3, 3);
+console.log(f)
---
+++ sololearn_increment_decrement3.js
@@ -0,0 +1,30 @@
+console.log('=======increment======')
+var a = 0
+var b = 10
+var a = b+=3
+console.log(a)
+console.log(b)
+
+
+var c = 0
+var d = 10
+var c = c += d + 3
+console.log(c)
+console.log(d)
+
+
+console.log('=======decrement======')
+var e = 0
+var f = 10
+var e = f-=3
+console.log(e)
+console.log(f)
+
+var g = 0
+var h = 10
+var g = g -= h -17
+
+console.log(g)
+console.log(h)
+
+ 
