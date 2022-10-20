b0e9050f1f836a20777fa4a6ebbb8c19a4693789
joedaffa <kelvin.daffa04@gmail.com>
2022-10-20 09:45:46 +0700

    10_20_202

---
+++ switchmakanan.js
@@ -0,0 +1,14 @@
+var menu = 1;
+
+switch(menu){
+    case 1:
+        console.log('basko')
+        break;
+    case 2:
+        console.log('mie ayam')
+        break;
+    case 3:
+        console.log('sate')
+        break;
+    
+}
