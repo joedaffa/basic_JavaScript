b4fd6444c29cf79ab1fb01d9035114f6055a8ffe
joedaffa <kelvin.daffa04@gmail.com>
2022-10-27 13:23:03 +0700

    Remed_ulangan_daffa_10_27_2022

---
+++ case.js
@@ -0,0 +1,13 @@
+var air = 1;
+ 
+switch (air){
+    case 1:
+        console.log('es teh manis')
+        break;
+    case 2:
+        console.log('hazelnut')
+        break;
+    case 3:
+        console.log('redvelvet')
+        break;
+}
